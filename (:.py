016b235c91d2be84351fda9c8860db737c8afb1e
campherro <84248861+campherro@users.noreply.github.com>
2024-07-17 09:57:48 -0500

    Create (:.py

---
+++ (:.py
@@ -0,0 +1,10 @@
+detirmining_status = input('Do you like pokémon? Y or N. ')
+if detirmining_status = 'Y':
+  print('weirdo.')
+else:
+  print('Good job. You passed!...but only phase 1!')
+  detirmining_status_2 = input("MTG or Yu-Gi-Oh?")
+  if detirmining_status_2 == 'MTG':
+    print('MTG 4EVA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+  else:
+    print('}:<')
